c9d5f68d1cbb9e7c2058b27bdf395581190db8f9
Oscar Rickovic <143960470+oscarRickovic@users.noreply.github.com>
2025-03-11 14:28:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Software_Evolution_TP_bots
---
+++ cliffsDelta.py
@@ -0,0 +1,31 @@
+# Taken from https://github.com/neilernst/cliffsDelta
+from __future__ import division
+
+
+def cliffsDelta(lst1, lst2):
+
+    """Returns Cliff's delta"""
+    m, n = len(lst1), len(lst2)
+    lst2 = sorted(lst2)
+    j = more = less = 0
+    for repeats, x in runs(sorted(lst1)):
+        while j <= (n - 1) and lst2[j] < x:
+            j += 1
+        more += j*repeats
+        while j <= (n - 1) and lst2[j] == x:
+            j += 1
+        less += (n - j)*repeats
+    d = (more - less) / (m*n)
+    return d
+
+
+def runs(lst):
+    """Iterator, chunks repeated values"""
+    for j, two in enumerate(lst):
+        if j == 0:
+            one, i = two, 0
+        if one != two:
+            yield j - i, one
+            i = j
+        one = two
+    yield j - i + 1, two
